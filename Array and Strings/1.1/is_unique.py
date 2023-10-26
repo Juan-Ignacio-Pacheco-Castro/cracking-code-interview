a6a477633b3deea9514f142a0ee7da65c4010d0f
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2023-10-26 11:40:18 -0600

    Added solution without Data Structure

--- Array and Strings/1.1/is_unique.py
+++ Array and Strings/1.1/is_unique.py
@@ -1,4 +1,4 @@
-to_be_checked = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
+to_be_checked = "abc"
 
 def is_unique(to_be_checked):
     hashmap = [0] * 255
@@ -7,11 +7,24 @@ def is_unique(to_be_checked):
     for i in to_be_checked:
         hashmap[ord(i)] += 1
         if(hashmap[ord(i)] == 2):
-            print(i)
             return False
+    return True
+
+def is_unique_without_data_structure(to_be_checked):
+    sorted_string = sorted(to_be_checked)
+    compare = None
+    for i in sorted_string:
+        if compare == i:
+            return False
+        compare = i
     return True
 
 if (is_unique(to_be_checked)):
     print("All are unique")
 else:
     print("There are repetitions")
+
+if (is_unique_without_data_structure(to_be_checked)):
+    print("Without Data Structure: All are unique")
+else:
+    print("Without Data Structure: There are repetitions")
